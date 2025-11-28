c19a628a5637a94bc094cdae073d5a09e9f4a6cd
Vipin Yadav <vipinyadav9m@gmail.com>
2025-11-28 14:24:39 +0530

    Remove node_modules from tracking and update .gitignore

--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_fa30a402._.js",
-  "static/chunks/_fa567f60._.js",
-  "static/chunks/[root-of-the-server]__8764df36._.css"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_page_tsx_f172d73c._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_1726d643._.js",
-  "static/chunks/components_83d52697._.js"
-],
-    source: "dynamic"
-});
